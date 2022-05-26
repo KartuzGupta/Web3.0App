107fe35ee45c0db4c4b2acfbb37d538866d1bc33
Kartikay Gupta <72855990+KartuzGupta@users.noreply.github.com>
2022-05-26 23:04:09 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xCac6a426BA87f16402a8f6929450F2Bf6619de88';
+export const contractAddress = '';
